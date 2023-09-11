598c672ca29f1fbf23ea041461e1f49531b68ed5
KristofGo <143124415+KristofGo@users.noreply.github.com>
2023-09-11 11:35:33 -0700

    Add files via upload

---
+++ Hello-World-Kris/String_Changer.java
@@ -0,0 +1,13 @@
+public class String_Changer {
+  public static void main(String[] args) {
+    String string1 = "I am a string :)";
+    String string2 = string1;
+    String string3 = "I am also a string";
+    boolean stringEqual;
+    if (string1 == string3) {
+        stringEqual = true;    
+    } else {
+        stringEqual = false;
+    }
+  }
+}
